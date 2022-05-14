f536f2afe9db3636effebd0fa3f48907d622ebb0
Yashwanth <97561730+yashwanth-eshwarappa@users.noreply.github.com>
2022-05-14 16:51:16 -0700

    Update PuzzleSolverUtils.java
    Refactored puzzle solver util class

--- PuzzleSolverUtils.java
+++ PuzzleSolverUtils.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import java.util.Set;
 
 public class PuzzleSolverUtils {
@@ -15,49 +14,12 @@ public class PuzzleSolverUtils {
 	}
 	
 	public static node[] getNodeNeighbors(int[][] puzzle) {
-		
+
 		int[] positions = getZeroPosition(puzzle);
 		int rowPosition =  positions[0];
 		int colPosition =  positions[1];
 		
 		node[] neighbors = new node[4];
-		
-//		for(int i=0;i<4;i++) {
-//			switch(i) {
-//			case 0://Move down
-//				if(rowPosition == 2){
-//					return null;
-//				}
-//				totalExpanded++;
-//				break;
-//			case 1://Move up
-//				if(rowPosition == 0){
-//					return null;
-//				}
-//				totalExpanded++;
-//				break;
-//			case 2://Move left
-//				if(colPosition == 0){
-//					return null;
-//				}
-//				totalExpanded++;
-//				break;
-//			case 3://Move right
-//				if(colPosition == 2){
-//					return null;
-//				}
-//				totalExpanded++;
-//				break;
-//			default: 
-//				break;
-//			}
-//		}
-		
-		
-		
-//		System.out.println("0->Down");
-		
-
 		
 		for(int i=0;i<4;i++) {
 			int temp;
@@ -73,8 +35,6 @@ public class PuzzleSolverUtils {
 					tempPuzzle = null;
 					break;
 				}
-//				System.out.println("Inside Move down");
-//				node.totalExpanded++;
 				temp = tempPuzzle[rowPosition + 1][colPosition];
 				tempPuzzle[rowPosition + 1][colPosition] = 0;
 				tempPuzzle[rowPosition][colPosition] = temp;
@@ -84,8 +44,6 @@ public class PuzzleSolverUtils {
 					tempPuzzle = null;
 					break;
 				}
-//				System.out.println("Inside Move Up");
-//				node.totalExpanded++;
 				temp = tempPuzzle[rowPosition - 1][colPosition];
 				tempPuzzle[rowPosition - 1][colPosition] = 0;
 				tempPuzzle[rowPosition][colPosition] = temp;
@@ -95,8 +53,6 @@ public class PuzzleSolverUtils {
 					tempPuzzle = null;
 					break;
 				}
-//				System.out.println("Inside Move left");
-//				node.totalExpanded++;
 				temp = tempPuzzle[rowPosition][colPosition - 1];
 				tempPuzzle[rowPosition][colPosition - 1] = 0;
 				tempPuzzle[rowPosition][colPosition] = temp;
@@ -106,8 +62,6 @@ public class PuzzleSolverUtils {
 					tempPuzzle = null;
 					break;
 				}
-//				System.out.println("Inside Move right");
-//				node.totalExpanded++;
 				temp = tempPuzzle[rowPosition][colPosition + 1];
 				tempPuzzle[rowPosition][colPosition + 1] = 0;
 				tempPuzzle[rowPosition][colPosition] = temp;
@@ -157,7 +111,6 @@ public class PuzzleSolverUtils {
 	
 	public int getManhattanDist(int[][] puzzle){
 		int dist = 0;
-
 		for(int i = 0; i < 3; ++i){
 			for(int j = 0; j < 3; ++j){
 				if(puzzle[i][j] != 0){
